32c3c05a5384bf3dd1aaa109b28f63e8235e662f
Localst4r <ionelapirvu14@gmail.com>
2026-07-17 02:57:35 +0300

    Delete run-all.js

--- run-all.js
+++
@@ -1,8 +0,0 @@
-// This file starts both the bot and the web dashboard in the same process
-// which is perfect for cloud hosting platforms like JustRunMyApp!
-
-console.log('[System] Starting Discord Bot...');
-require('./bot/index.js');
-
-console.log('[System] Starting Web Dashboard...');
-require('./dashboard/server.js');
